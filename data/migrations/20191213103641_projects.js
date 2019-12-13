9fe79e172db5d14a66fd02560f35a3f88eb20e9d
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-13 10:47:33 -0600

    create migration and tables projects tasks and resources

---
+++ data/migrations/20191213103641_projects.js
@@ -0,0 +1,60 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("projects", tbl => {
+      tbl.increments();
+
+      tbl.string("name", 255).notNullable();
+
+      tbl.string("description", 255);
+
+      tbl.boolean("completed", false).notNullable();
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments();
+
+      tbl.string("description", 255).notNullable();
+
+      tbl.string("notes", 255);
+
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+      tbl.boolean("completed", false).notNullable();
+    })
+    .createTable("resources", tbl => {
+      tbl.increments();
+
+      tbl.string("name", 255).notNullable();
+
+      tbl.string("description", 255);
+    })
+    .createTable("project_resource", tbl => {
+      tbl.primary(["project_id", "resource_id"]);
+
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex) {};
