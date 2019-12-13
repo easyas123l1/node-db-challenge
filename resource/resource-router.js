2c5bcd1a52c3bcf11b212a350d5a8cca009a5958
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-13 14:26:58 -0600

    post resource endpoint

--- resource/resource-router.js
+++ resource/resource-router.js
@@ -4,6 +4,7 @@ const Resource = require("./resource-model");
 
 const router = express.Router();
 
+// get all resources
 router.get("/", (req, res) => {
   Resource.getResources()
     .then(resource => {
@@ -15,6 +16,7 @@ router.get("/", (req, res) => {
     });
 });
 
+// get resource by id
 router.get("/:id", (req, res) => {
   Resource.getResourceId(req.params.id)
     .then(resource => {
@@ -24,6 +26,23 @@ router.get("/:id", (req, res) => {
       console.log(err);
       res.status(500).json({ message: "failed to get resource by id" });
     });
+});
+
+router.post("/", (req, res) => {
+  const data = req.body;
+
+  if (data.name) {
+    Resource.addResource(data)
+      .then(resource => {
+        res.status(201).json(resource);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "failed to create a new resource" });
+      });
+  } else {
+    res.status(400).json({ message: "resource must have a name" });
+  }
 });
 
 module.exports = router;
